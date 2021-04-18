b36d99a3223e2ae5f681ece7c385db6afdd4e5fe
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-04-18 15:18:06 +0200

    fix guest

--- app/Http/Middleware/CheckGuest.php
+++ app/Http/Middleware/CheckGuest.php
@@ -16,7 +16,6 @@ class CheckGuest
      */
     public function handle($request, Closure $next)
     {
-
         if(auth()->user()){
             return $next($request);
         }else{
@@ -24,7 +23,7 @@ class CheckGuest
             if($gusetkey == '$2y$12$ZtgKLOyfvyXH33JE67Ei0.qupt771t62d21M4/OJumBmsZ1bexxpCPiuhfdRKO'){
                 return $next($request);
             }
-            $response = APIHelpers::createApiResponse(true , 401 ,  'توكن زائر خاطيء' , null );
+            $response = APIHelpers::createApiResponse(true , 401 ,  'Guest token wrong', 'توكن زائر خاطيء', null,$request->lang );
             return response()->json($response , 401);
         }
     }
