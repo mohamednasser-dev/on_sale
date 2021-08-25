0248eddc4176bf997cdac67cd506b0789ccff2e1
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-08-25 14:55:52 +0200

    sadss

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -283,6 +283,7 @@ class ProductController extends Controller
             ->map(function ($ads) use ($lang) {
                 $ads->price = number_format((float)($ads->price), 3);
                 $ads->time = APIHelpers::get_month_year($ads->created_at, $lang);
+                $ads->views = Product_view::where('product_id', $ads->id)->get()->count();
                 return $ads;
             });
 
